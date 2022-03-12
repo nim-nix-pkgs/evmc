cba49b4b00922b9f2794403058b303f12b01f6bd
hugosenari <hugosenari@gmail.com>
2022-03-12 02:09:02 -0300

    chore: re index evmc

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''A wrapper for the The Ethereum EVMC library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-evmc-master.flake = false;
+  inputs.src-evmc-master.owner = "status-im";
+  inputs.src-evmc-master.ref   = "refs/heads/master";
+  inputs.src-evmc-master.repo  = "nim-evmc";
+  inputs.src-evmc-master.type  = "github";
+  
+  inputs."stew".dir   = "nimpkgs/s/stew";
+  inputs."stew".owner = "riinr";
+  inputs."stew".ref   = "flake-pinning";
+  inputs."stew".repo  = "flake-nimble";
+  inputs."stew".type  = "github";
+  inputs."stew".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."stew".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-evmc-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-evmc-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
